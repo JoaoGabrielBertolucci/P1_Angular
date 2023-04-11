9e3d45a0e16c3f1f14c574f02c22113587f8b4f3
williamfernandessilva <joaobertolucci6@gmail.com>
2023-04-11 10:45:03 -0300

    semi_final

--- P1_AngularConv/src/app/conversor/conversor.component.ts
+++ P1_AngularConv/src/app/conversor/conversor.component.ts
@@ -7,12 +7,85 @@ import { Component } from '@angular/core';
 })
 export class ConversorComponent {
 M1: number = 0;
-M2: number = 0;
-indx: any;
+convert: any;
+
+indx1: any;
+indx2: any;
 
   Conv(){
 
+    if (this.indx1 == "Dolar") {
+      
+      if (this.indx2 == "Dolar"){
+        alert("Não pode ser a mesma moeda!");
+      } 
+      
+      if (this.indx2 == "Euro") 
+      {
+        
+       this.convert = this.M1 * 0.96
+
+      }
+
+      if (this.indx2 == "Real") 
+      {
+        
+       this.convert = this.M1 * 5
+
+      }
+
+    }
+
+
+    //---------------------------------------
+
+    if (this.indx1 == "Euro") {
+      
+      if (this.indx2 == "Euro"){
+        alert("Não pode ser a mesma moeda!");
+      } 
+      
+      if (this.indx2 == "Dolar") 
+      {
+        
+       this.convert = this.M1 * 1.09;
+
+      }
+
+      if (this.indx2 == "Real") 
+      {
+        
+       this.convert = this.M1 * 5.46;
+
+      }
+
+    }
     
+    //----------------------------------------------
+  
+    
+    if (this.indx1 == "Real") {
+      
+      if (this.indx2 == "Real"){
+        alert("Não pode ser a mesma moeda!");
+      } 
+      
+      if (this.indx2 == "Dolar") 
+      {
+        
+       this.convert = this.M1 * 0.20;
+
+      }
+
+      if (this.indx2 == "Euro") 
+      {
+        
+       this.convert = this.M1 * 0.18;
+
+      }
+
+    }
+
   }
 
 }
